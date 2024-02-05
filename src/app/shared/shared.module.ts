f6246ef105edea96036a524838a7f3a4ef727366
Eizval <damian.mermod@mac.com>
2024-02-05 15:59:04 +0100

    Understanding angular better

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ExampleAComponent } from './example-a/example-a.component';
 import { ExampleBComponent } from './example-b/example-b.component';
 import { ExampleCComponent } from './example-c/example-c.component';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbAlertModule
   ],
   declarations: [
     ExampleAComponent,
